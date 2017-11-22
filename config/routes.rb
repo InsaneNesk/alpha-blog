146c94888f3215bcf19ec5c54bbc4d66888cb04e
insanenesk <chris@matecrate.de>
2017-11-22 15:28:18 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'frontend#home'
-  get 'frontend/about', to: 'frontend#about'
+  get 'about', to: 'frontend#about'
 
 end
